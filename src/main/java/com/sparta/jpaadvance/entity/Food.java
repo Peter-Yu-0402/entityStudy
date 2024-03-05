f80d9b1eb102b49e53e727c9c5b1822787f150e5
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-03-05 18:26:55 +0900

    @OneToOne 양방향 및 조회 테스트

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -16,6 +16,9 @@ public class Food {
     private double price;
 
     @OneToOne
+    // 외래 키 주인만이 외래 키를 등록, 수정, 삭제 할 수 있으며, 주인이 아닌 쪽은 오직 외래 키를 읽기만 가능하다.
+    // @JoinColumn은 외래 키의 주인이 활용하는 애터네이션이다.
     @JoinColumn(name = "user_id")
+    // 상대 엔티티의 데이터타입과 필드명을 적는다.
     private User user;
 }
